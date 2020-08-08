3faa96806dddd1c55f8bd4fbff74aa240e6e4a4b
bigmud <bigmudhaijian@gmail.com>
2020-08-08 16:01:57 +0800

    test branch

---
+++ Java/12.java
@@ -0,0 +1,15 @@
+package Java;
+
+import java.util.Scanner;
+
+class Main{
+
+    public static void Main(String[] args){
+
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()){
+
+        }
+        in.close();
+    }
+}
